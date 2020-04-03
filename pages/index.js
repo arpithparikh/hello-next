14c6b5c4869203c4e7814ca47c0a19a4cd2bfded
arpithparikh <arpithparikh@gmail.com>
2020-04-03 16:02:33 +0530

    First commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+
+import { ZoomMtg } from '@zoomus/websdk'
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+const Index = () => (
+  
+    <div>
+      <p>Hello Next.js</p>
+    </div>
+  );
+  
+  export default Index;
